fdc292d1191433db9ba16db3d911f45bdeb12565
TJK98 <wtj1998@naver.com>
2025-08-29 14:31:25 +0900

    14. 카테고리 클릭 이벤트 - `useNavigate`와 `useSearchParams` 훅을 사용하여 새로 고침 없이 카테고리 필터 적용 - 카테고리 클릭 핸들러 함수 구현 및 사이드바 버튼에 적용 - 라우터 설정 오타 수정 (idex -> index)

--- src/components/BlogSideBar.jsx
+++ src/components/BlogSideBar.jsx
@@ -1,7 +1,26 @@
 import styles from './BlogSideBar.module.scss';
 import {categories} from '../dummy-data/dummy-post.js';
+import {useNavigate, useSearchParams} from "react-router-dom";
 
 const BlogSideBar = () => {
+
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    // use~~가 붙은 건 훅이라고 부르는데 훅은 component 바로 아래에서만 호출해야 된다.
+    // 새로 고침 없이 리다이랙션을 지원하는 훅
+    const navigate = useNavigate();
+
+    // const handelCategoryClick = e => {
+    const handelCategoryClick = (category) => {
+        // 사이드바에 있는 카테고리를 누르면 목록으로 이동하면서 카테고리를 보여줘야 함.
+        // location.href = `/blog`; // 새로 고침이 일어나서 쓰면 안 된다.
+        // navigate(`/blog?categoty=${category}`); // location.href = `/blog`;와 같은 기능. 하지만 새로 고침이 일어나지 않는다.
+        navigate(`/blog`);
+        setSearchParams(prev => {
+            prev.set('category', category);
+            return prev;
+        });
+    };
 
     return (
         <aside className={styles.sidebar}>
@@ -11,6 +30,8 @@ const BlogSideBar = () => {
                     <li key={category.id}>
                         <button
                             className={`${styles.categoryButton}`}
+                            // onClick={handelCategoryClick}
+                            onClick={e => handelCategoryClick(category.id)} // 직접 함수를 재호출하면 파라미터를 넣을 수 있다.
                         >
                             {category.name}
                             <span className={styles.count}>
--- src/routes/router-config.jsx
+++ src/routes/router-config.jsx
@@ -43,7 +43,7 @@ export const router = createBrowserRouter([
                 element: <BlogLayout />,
                 children: [
                     {
-                        idex: true,
+                        index: true,
                         element: <BlogPage />
                     },
                     {

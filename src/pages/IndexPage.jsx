00c0b64f14bfc7990ed2f02052a33a0796d61cb7
TJK98 <wtj1998@naver.com>
2025-08-29 09:39:15 +0900

    1. 첫번째 라우팅 하기 - 라우터 설정 및 페이지 컴포넌트 추가

---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,13 @@
+// components와 달리 pages 파일은 좀 더 큰 페이지들의 묶음이고
+// components은 조금 더 작은 페이지들의 묶음이다
+import React from 'react';
+
+const IndexPage = () => {
+    return (
+        <h1>
+            Index 페이지 입니다.
+        </h1>
+    );
+};
+
+export default IndexPage;
--- src/routes/router-config.jsx
+++ src/routes/router-config.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import {RouterProvider} from 'react-router-dom';
-import {router} from './routes/router-config.jsx';
-/*
+// 대문자로 시작하지 않고 소문자로 시작하면 일반 js
+
+import {createBrowserRouter} from 'react-router-dom';
+import IndexPage from '../pages/IndexPage.jsx';
+import BlogPage from '../pages/BlogPage.jsx';
+import AboutPage from '../pages/AboutPage.jsx';
+
+
 // 라우터 설정을  props로 전달
-const router = createBrowserRouter([
+export const router = createBrowserRouter([
     {
         path: `/`,
         element:<IndexPage />
@@ -17,12 +21,3 @@ const router = createBrowserRouter([
         element:<AboutPage />
     },
 ]); // 배열을 넣고 그 안에는 객체를 넣어야 한다. / 다음에 어떤 단어가 오면 그 페이지로 이동
-*/
-
-const App = () => {
-    return (
-        <RouterProvider router={router} /> //
-    );
-};
-
-export default App;

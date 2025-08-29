5de1d32c8eee1eed6a607f4264098a1d0a5ef856
TJK98 <wtj1998@naver.com>
2025-08-29 11:47:58 +0900

    10. 카테고리 필터링 기능 추가 - BlogFilter 컴포넌트에 카테고리 선택 시 쿼리스트링 업데이트 로직 구현 - dummy-data의 카테고리를 기반으로 옵션 동적 생성

--- src/components/BlogFilter.jsx
+++ src/components/BlogFilter.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import styles from './BlogFilter.module.scss';
+import {categories} from "../dummy-data/dummy-post.js";
+import {useSearchParams} from "react-router-dom";
 
 const BlogFilter = () => {
+
+    // 쿼리 스트링 생성 기능
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    // 카테고리 옵션 선택 이벤트
+    const handelCategotyChange = e => {
+        // console.log(`category가 선택 됨: `, e.target.value);
+        // 쿼리 스트링으로 ?category=xxx
+        setSearchParams(prev => {
+            console.log(`prev: `, prev);
+            prev.set(`category`, e.target.value);
+            return prev;
+        });
+    }
+
     return (
         <div className={styles.filter}>
-            <select>
-                <option>리액트</option>
-                <option>노드JS</option>
+            <select onChange={handelCategotyChange} value={searchParams.get(`category`) || `all`}>
+                {/*직접 작성하는 게 아닌 카테고리에서 직접 꺼내와서 옵션을 걸어줘야 한다.*/}
+                {/*<option>리액트</option>*/}
+                {/*<option>노드JS</option>*/}
+                {categories.map(ct => <option key={ct.id} value={ct.id}>{ct.name}</option>)}
             </select>
 
             <select>

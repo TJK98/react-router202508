49fe16e00555dd04f6bda0c8186e2c3fbb6776cb
TJK98 <wtj1998@naver.com>
2025-08-29 10:12:59 +0900

    3. 레이아웃 사용하기 - RootLayout 컴포넌트 생성 및 적용 - Outlet을 이용한 동적 콘텐츠 렌더링 처리 - MainNav 스타일 색상 수정 (검정 → 흰색)

--- src/routes/router-config.jsx
+++ src/routes/router-config.jsx
@@ -4,11 +4,12 @@ import {createBrowserRouter} from 'react-router-dom';
 import IndexPage from '../pages/IndexPage.jsx';
 import BlogPage from '../pages/BlogPage.jsx';
 import AboutPage from '../pages/AboutPage.jsx';
+import RootLayout from "../layouts/RootLayout.jsx";
 
 
 // 라우터 설정을  props로 전달
 export const router = createBrowserRouter([
-    {
+/*    {
         path: `/`,
         element:<IndexPage />
     },
@@ -20,4 +21,25 @@ export const router = createBrowserRouter([
         path: `/about`,
         element:<AboutPage />
     },
+*/
+    {
+        path: `/`,
+        element: <RootLayout/>,
+        // children -> Layout의 Outlet 부분을 무엇으로 바꿀지 설정
+        children: [
+            {
+                // path: '', // /(슬래쉬를) 생략해도 된다. 위에 부모에서 /를 설정 했기 때문에.
+                index: true, // path가 빈문자열이면 이렇게 대체할 수도 있다.
+                element: <IndexPage />
+            },
+            {
+                path: 'blog',
+                element: <BlogPage />
+            },
+            {
+                path: 'about',
+                element: <AboutPage />
+            },
+        ]
+    }
 ]); // 배열을 넣고 그 안에는 객체를 넣어야 한다. / 다음에 어떤 단어가 오면 그 페이지로 이동
